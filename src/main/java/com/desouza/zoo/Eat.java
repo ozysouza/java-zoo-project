3ff68de14130aca808ba2a96898ddb689bca3799
ozysouza <ozysouzaa@gmail.com>
2026-03-11 21:20:14 -0400

    Create Interfaces for Eat and Walk

---
+++ src/main/java/com/desouza/zoo/Eat.java
@@ -0,0 +1,6 @@
+package main.java.com.desouza.zoo;
+
+public interface Eat {
+    public void eatingFood();
+    public void eatingCompleted();
+}
---
+++ src/main/java/com/desouza/zoo/Walk.java
@@ -0,0 +1,4 @@
+package main.java.com.desouza.zoo;
+
+public interface Walk {
+}
