3cdef84655c94fc4b0b8ce799d7ec657e70e0253
ozysouza <ozysouzaa@gmail.com>
2026-03-11 21:27:47 -0400

    Create Animal class, implements Eat interface

---
+++ src/main/java/com/desouza/zoo/Animal.java
@@ -0,0 +1,55 @@
+package main.java.com.desouza.zoo;
+
+abstract public class Animal implements Eat {
+
+    private String nameOfAnimal;
+    private int weight;
+    private int height;
+    private int age;
+
+    public Animal() {
+        nameOfAnimal = "Unknown Animal";
+    }
+
+    public Animal(String nameOfAnimal) {
+        this.nameOfAnimal = nameOfAnimal;
+    }
+
+    public String getNameOfAnimal() {
+        return nameOfAnimal;
+    }
+
+    public void setNameOfAnimal(String nameOfAnimal) {
+        this.nameOfAnimal = nameOfAnimal;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public void eatingFood() {
+        System.out.println("The animal: " + nameOfAnimal + " is eating.");
+    }
+
+}
